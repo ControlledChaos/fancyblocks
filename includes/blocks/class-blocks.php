c44442ee594bade7d1103c9619d620aa8bf3118b
ControlledChaos <greg@ccdzine.com>
2018-08-01 17:11:20 -0800

    Added fancyBox & started blocks directory

---
+++ includes/blocks/class-blocks.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Start stacking blocks.
+ *
+ * @package    FancyBlocks
+ * @subpackage Includes
+ *
+ * @since      1.0.0
+ * @author     Greg Sweet <greg@ccdzine.com>
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Start stacking blocks.
+ *
+ * @since  1.0.0
+ * @access public
+ */
+class FancyBlocks_Blocks {
+
+	/**
+	 * Constructor method.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @return self
+	 */
+	public function __construct() {
+
+		$this->dependencies();
+
+	}
+
+	/**
+	 * Load the required dependencies for this class.
+	 *
+	 * @since  1.0.0
+	 * @access private
+	 * @return void
+	 */
+	private function dependencies() {}
+
+}
+
+$fancyblocks_blocks = new FancyBlocks_Blocks();
